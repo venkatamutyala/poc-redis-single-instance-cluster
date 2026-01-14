ae50b6e2b66ef4165156c3a00cb8d12153693a60
null <antonio.rodriguez@glueops.dev>
2026-01-14 02:23:06 +0000

    fix: add port for tls and non tls

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 1. Start Redis in the background
 redis-server \
-  --tls-port 6379 --port 0 \
+  --tls-port 6380 --port 6379 \
   --tls-cert-file /certs/redis.crt \
   --tls-key-file /certs/redis.key \
   --tls-ca-cert-file /certs/ca.crt \
